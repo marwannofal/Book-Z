c119483264a58832bfc45838d84eb4d6a6ca88ae
marwannofal <135698263+marwannofal@users.noreply.github.com>
2024-06-02 21:53:51 +0300

    the 11th day work

--- API/Entities/Book.cs
+++ API/Entities/Book.cs
@@ -5,9 +5,7 @@ namespace API.Entities
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        // public string Tistle { get; set; }
-        
+        public string Title { get; set; }        
         public Condition Condition { get; set; }
         public string Description { get; set; }
         public Availability Availability { get; set; }
